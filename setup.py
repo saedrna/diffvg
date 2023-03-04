6771fcf51bb568b867d920ab1d96405dd26d5505
Han Hu <han.hu@swjtu.edu.cn>
2023-03-04 17:06:44 +0800

    refactor build with pytorch extension

--- setup.py
+++ setup.py
@@ -14,63 +14,44 @@ from setuptools.command.install import install
 from distutils.sysconfig import get_config_var
 from distutils.version import LooseVersion
 
-class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir, build_with_cuda):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
-        self.build_with_cuda = build_with_cuda
 
-class Build(build_ext):
-    def run(self):
-        try:
-            out = subprocess.check_output(['cmake', '--version'])
-        except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
+from setuptools import setup, Extension
+from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 
-        super().run()
+CONDA_PREFIX = os.environ['CONDA_PREFIX']
 
-    def build_extension(self, ext):
-        if isinstance(ext, CMakeExtension):
-            extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-            info = get_paths()
-            include_path = info['include']
-            cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                          '-DPYTHON_LIBRARY=' + get_config_var('LIBDIR'),
-                          '-DPYTHON_INCLUDE_PATH=' + include_path]
+Extension = CUDAExtension
+libraries = []
+include_dirs = []
+CC_FLAGS = []
+NVCC_FLAGS = []
 
-            cfg = 'Debug' if self.debug else 'Release'
-            build_args = ['--config', cfg]
+libraries.append('cudart')
+CC_FLAGS += ["-O3"]
+NVCC_FLAGS += ["-O3", "-Xcompiler=-fno-gnu-unique"]
 
-            if platform.system() == "Windows":
-                cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir),
-                               '-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-                if sys.maxsize > 2**32:
-                    cmake_args += ['-A', 'x64']
-                build_args += ['--', '/m']
-            else:
-                cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-                build_args += ['--', '-j8']
+extension = Extension(
+    name='diffvg',
+    sources=[
+        'atomic.cpp',
+        'color.cpp',
+        'diffvg.cu',
+        'parallel.cpp',
+        'shape.cpp',
+        'scene.cu'
+    ],
+    include_dirs=[
+        os.path.join(CONDA_PREFIX, 'include')],
+    extra_compile_args={'cxx': CC_FLAGS, 'nvcc': NVCC_FLAGS},
+    extra_link_args=['-lcudart'],
+    libraries=libraries
+)
 
-            if ext.build_with_cuda:
-                cmake_args += ['-DDIFFVG_CUDA=1']
-            else:
-                cmake_args += ['-DDIFFVG_CUDA=0']
-
-            env = os.environ.copy()
-            env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                                  self.distribution.get_version())
-            if not os.path.exists(self.build_temp):
-                os.makedirs(self.build_temp)
-            subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-            subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
-        else:
-            super().build_extension(ext)
 
 torch_spec = importlib.util.find_spec("torch")
 tf_spec = importlib.util.find_spec("tensorflow")
 packages = []
-build_with_cuda = False
+build_with_cuda = True
 if torch_spec is not None:
     packages.append('pydiffvg')
     import torch
@@ -85,15 +66,13 @@ if tf_spec is not None and sys.platform != 'win32':
 if len(packages) == 0:
     print('Error: PyTorch or Tensorflow must be installed. For Windows platform only PyTorch is supported.')
     exit()
-# Override build_with_cuda with environment variable
-if 'DIFFVG_CUDA' in os.environ:
-    build_with_cuda = os.environ['DIFFVG_CUDA'] == '1'
 
-setup(name = 'diffvg',
-      version = '0.0.1',
-      install_requires = ["svgpathtools"],
-      description = 'Differentiable Vector Graphics',
-      ext_modules = [CMakeExtension('diffvg', '', build_with_cuda)],
-      cmdclass = dict(build_ext=Build, install=install),
-      packages = packages,
-      zip_safe = False)
+
+setup(name='diffvg',
+      version='0.0.1',
+      install_requires=["svgpathtools"],
+      description='Differentiable Vector Graphics',
+      ext_modules=[extension],
+      cmdclass=dict(build_ext=BuildExtension.with_options(use_ninja=True), install=install),
+      packages=packages,
+      zip_safe=False)
